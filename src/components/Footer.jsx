4da56144c68a9208b32a43d123c61a8e7a4a9cce
Dishant Singh <121028242+DishantSinghDev@users.noreply.github.com>
2023-08-02 06:27:48 +0000

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright &copy; 2016 - {new Date().getFullYear()} DishIs. All Rights Reserved. Developed and Deployed by <Link to="https://github.com/DishantSinghDev">Dishant Singh</Link>
+        Copyright &copy; 2016 - {new Date().getFullYear()} <a href="https://dishis.in">DishIs</a>. All Rights Reserved. Developed and Deployed by <Link to="https://github.com/DishantSinghDev">Dishant Singh</Link>
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
